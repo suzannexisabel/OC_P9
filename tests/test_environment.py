4c8abbf209713d574a95a1a89131189246049e75
Suzanne Endtz <suzanne.endtz@gmail.com>
2026-08-27 14:05:19 -0300

    fix CI environment test

--- tests/test_environment.py
+++ tests/test_environment.py
@@ -5,13 +5,9 @@ from langchain_mistralai import ChatMistralAI
 from langchain_core.prompts import ChatPromptTemplate
 from mistralai.client import Mistral
 
-from ragas.llms import LangchainLLMWrapper
-
-
 def test_imports():
     assert faiss is not None
     assert FastAPI is not None
     assert ChatMistralAI is not None
     assert ChatPromptTemplate is not None
     assert Mistral is not None
-    assert LangchainLLMWrapper is not None
